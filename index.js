ed234adba9f41ada1fddedc7c0700d6aedbba42f
abyusuft <imabuyusuf@gmail.com>
2022-05-07 23:23:10 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,9 +10,33 @@ app.use(cors());
 app.use(express.json());
 
 
+// MongoDB 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kqnws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        client.connect();
+        const collection = client.db("water").collection("product");
+
+
+
+
+
+    }
+    finally {
+        // client.close();
+    }
+}
+
+
+
+
 // App Root 
 app.get('/', (req, res) => {
-    res.send('water Warehouse Running');
+    res.send('Water warehouse running');
 })
 
 // Port Listening 
